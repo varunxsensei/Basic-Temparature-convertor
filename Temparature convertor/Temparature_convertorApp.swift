b48bf395ce085d4c058393baab4cf7180bc209fe
varunxsensei <varunxai27@gmail.com>
2025-05-29 08:53:14 +0530

    Initial Commit

---
+++ Temparature convertor/Temparature_convertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temparature_convertorApp.swift
+//  Temparature convertor
+//
+//  Created by Varun Saxena on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Temparature_convertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
